a0a902a25b30c99ea25e5d1e1bbbad47c4d7899a
Novy Chariz <unvyil@gmail.com>
2024-11-07 08:40:08 +0800

    LAB11

--- Tablac_Lab11.py
+++ Tablac_Lab11.py
@@ -7,13 +7,14 @@ loop = True
 
 # INPUT GRADES
 while loop == True:
-        grade_input = input(f'Student {num_student}: ')
-        if grade_input.isdigit():
-            grade_input = int(grade_input)
+        grade_input = input(f"Enter the grade for Student {num_student}: ")
+        if grade_input.replace(".", "", 1).isdigit() and grade_input.count(".") <= 1:
+            grade_input = float(grade_input)
             if grade_input >= 40 and grade_input <= 100:
                 grades.append(grade_input)
                 num_student += 1
                 continue
+    
             else:
                 print("Error: Invalid input (must be between 40 and 100). Try again.")
                 continue
@@ -28,7 +29,7 @@ else:
             total += grade
             average_grade = total / len(grades)
         else:
-            print(f"The average grade of the students: {average_grade:2f}")
+            print(f"The average grade of the students: {average_grade:.2f}")
         for grade in grades:
             if grade >= 75:
                 students_passed += 1
