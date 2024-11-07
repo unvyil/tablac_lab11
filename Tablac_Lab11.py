23098682397828f4978b7903bf53c0f28e8cc13d
audthn <altheaudtuhan@gmail.com>
2024-11-07 08:18:36 +0800

    LAB9

---
+++ Tablac_Lab11.py
@@ -0,0 +1,40 @@
+# VARIABLES
+grades = []
+num_student = 1
+students_passed = 0 
+total = 0
+loop = True
+
+# INPUT GRADES
+while loop == True:
+        grade_input = input(f'Student {num_student}: ')
+        if grade_input.isdigit():
+            grade_input = int(grade_input)
+            if grade_input >= 40 and grade_input <= 100:
+                grades.append(grade_input)
+                num_student += 1
+                continue
+            else:
+                print("Error: Invalid input (must be between 40 and 100). Try again.")
+                continue
+        elif grade_input.upper() == "DONE":
+            loop = False
+        else:
+            print("Error: Invalid input. Please enter a valid number.")
+            continue
+else:
+    if grades != []:
+        for grade in grades:
+            total += grade
+            average_grade = total / len(grades)
+        else:
+            print(f"The average grade of the students: {average_grade:2f}")
+        for grade in grades:
+            if grade >= 75:
+                students_passed += 1
+        else:
+            print(f"Number of students who passed: {students_passed}")
+            passing_percentage = (students_passed / len(grades)) * 100
+            print(f"Passing percentage: {passing_percentage:.2f}%")
+    else:
+        print("No grades were entered.")
